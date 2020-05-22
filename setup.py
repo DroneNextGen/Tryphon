e7fcb562bf87865cb317ea19bf9ba8134e63f4f7
Clément Trassoudaine <clement.trassoudaine@gmail.com>
2020-05-22 12:09:24 +0200

    Add python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tryphon',
+    version='0.1.0',    
+    description='A simple, robust and lightweight communication protocol',
+    url='https://github.com/DroneNextGen/Tryphon',
+    author='Clément Trassoudaine',
+    author_email='clement.trassoudaine@eurecom.fr',
+    license='MIT',
+    packages=['tryphon'],
+    install_requires=['ctypes'],
+)
